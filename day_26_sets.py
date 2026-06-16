37a909ff1aa317f530c24be69e92482bafc9fede
Jagrit Mandora <jagritmandora@gmail.com>
2026-06-17 00:23:50 +0530

    collections tuple, set and lists

---
+++ day_26_sets.py
@@ -0,0 +1,21 @@
+# set{} : unordered and immutable. but add/ remove allowed. no duplicates are allowed.
+
+# for sets we can not use indexes as set does not have order.
+
+carsset = {"maruti","honda","toyota","suzuki","honda"}
+
+#print(dir(carsset))
+
+print(help(carsset))
+
+#carsset.add("tata")
+
+#carsset.remove("maruti")
+
+#print(len(carsset))
+
+#carsset.pop()
+
+#carsset.clear()
+
+print(carsset)
---
+++ day_27_tuple.py
@@ -0,0 +1,20 @@
+# tuples() : ordered and unchangable . duplicates allowed and FASTER
+
+# it does not have much functions.
+
+cars = ("maruti","honda","toyota","suzuki","honda")
+
+#print(len(cars))
+
+#print(cars[0])
+
+#print("maruti" in cars)
+
+#print(cars.count("maruti"))
+
+#print(dir(cars))
+
+#print(help(cars))
+
+for i in cars:
+    print(i)
